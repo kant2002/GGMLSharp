5826c9583edea0231a2d5d3814b82ab41f069405
Andrii Kurdiumov <kant2002@gmail.com>
2023-06-15 23:53:34 +0600

    Increase stack size to 4MB and pass two more phases in test1

--- Test1/Program.cs
+++ Test1/Program.cs
@@ -115,98 +115,98 @@ unsafe
 
     ///////////////////////////////////////////////////////////////
 
-    //{
-    //    ggml_tensor * x1 = ggml_new_tensor_1d(ctx0, ggml_type.GGML_TYPE_F32, 1);
-    //    ggml_tensor * x2 = ggml_new_tensor_1d(ctx0, ggml_type.GGML_TYPE_F32, 1);
+    {
+        ggml_tensor* x1 = ggml_new_tensor_1d(ctx0, ggml_type.GGML_TYPE_F32, 1);
+        ggml_tensor* x2 = ggml_new_tensor_1d(ctx0, ggml_type.GGML_TYPE_F32, 1);
 
-    //    ggml_set_param(ctx0, x1);
-    //    ggml_set_param(ctx0, x2);
+        ggml_set_param(ctx0, x1);
+        ggml_set_param(ctx0, x2);
 
-    //    ggml_tensor * y = ggml_mul(ctx0, ggml_add(ctx0, ggml_mul(ctx0, x1, x1), ggml_mul(ctx0, x1, x2)), x1);
+        ggml_tensor* y = ggml_mul(ctx0, ggml_add(ctx0, ggml_mul(ctx0, x1, x1), ggml_mul(ctx0, x1, x2)), x1);
 
-    //    ggml_cgraph gf = ggml_build_forward(y);
-    //    ggml_cgraph gb = ggml_build_backward(ctx0, &gf, false);
+        ggml_cgraph gf = ggml_build_forward(y);
+        ggml_cgraph gb = ggml_build_backward(ctx0, &gf, false);
 
-    //    ggml_set_f32(x1, 3.0f);
-    //    ggml_set_f32(x2, 4.0f);
+        ggml_set_f32(x1, 3.0f);
+        ggml_set_f32(x2, 4.0f);
 
-    //    ggml_graph_reset(&gf);
-    //    ggml_set_f32(y->grad, 1.0f);
+        ggml_graph_reset(&gf);
+        ggml_set_f32(y->grad, 1.0f);
 
-    //    ggml_graph_compute(ctx0, &gb);
+        ggml_graph_compute(ctx0, &gb);
 
-    //    Console.WriteLine("y      = {0:F6}", ggml_get_f32_1d(y, 0));
-    //    Console.WriteLine("df/dx1 = {0:F6}", ggml_get_f32_1d(x1->grad, 0));
-    //    Console.WriteLine("df/dx2 = {0:F6}", ggml_get_f32_1d(x2->grad, 0));
+        Console.WriteLine("y      = {0:F6}", ggml_get_f32_1d(y, 0));
+        Console.WriteLine("df/dx1 = {0:F6}", ggml_get_f32_1d(x1->grad, 0));
+        Console.WriteLine("df/dx2 = {0:F6}", ggml_get_f32_1d(x2->grad, 0));
 
-    //    Debug.Assert(ggml_get_f32_1d(y, 0)        == 63.0f);
-    //    Debug.Assert(ggml_get_f32_1d(x1->grad, 0) == 51.0f);
-    //    Debug.Assert(ggml_get_f32_1d(x2->grad, 0) == 9.0f);
+        Debug.Assert(ggml_get_f32_1d(y, 0) == 63.0f);
+        Debug.Assert(ggml_get_f32_1d(x1->grad, 0) == 51.0f);
+        Debug.Assert(ggml_get_f32_1d(x2->grad, 0) == 9.0f);
 
-    //    ggml_graph_dump_dot(&gf, null, "test1-3-forward.dot");
-    //    ggml_graph_dump_dot(&gb, &gf,  "test1-3-backward.dot");
-    //}
+        ggml_graph_dump_dot(&gf, null, "test1-3-forward.dot");
+        ggml_graph_dump_dot(&gb, &gf, "test1-3-backward.dot");
+    }
 
     ///////////////////////////////////////////////////////////////
 
-    //{
-    //    ggml_tensor * x1 = ggml_new_tensor_1d(ctx0, ggml_type.GGML_TYPE_F32, 1);
-    //    ggml_tensor * x2 = ggml_new_tensor_1d(ctx0, ggml_type.GGML_TYPE_F32, 1);
-    //    ggml_tensor * x3 = ggml_new_tensor_1d(ctx0, ggml_type.GGML_TYPE_F32, 1);
+    {
+        ggml_tensor* x1 = ggml_new_tensor_1d(ctx0, ggml_type.GGML_TYPE_F32, 1);
+        ggml_tensor* x2 = ggml_new_tensor_1d(ctx0, ggml_type.GGML_TYPE_F32, 1);
+        ggml_tensor* x3 = ggml_new_tensor_1d(ctx0, ggml_type.GGML_TYPE_F32, 1);
 
-    //    ggml_set_param(ctx0, x1);
-    //    ggml_set_param(ctx0, x2);
-    //    ggml_set_param(ctx0, x3);
+        ggml_set_param(ctx0, x1);
+        ggml_set_param(ctx0, x2);
+        ggml_set_param(ctx0, x3);
 
-    //    ggml_tensor * y = ggml_mul(ctx0, ggml_mul(ctx0, ggml_mul(ctx0, x1, x1), ggml_mul(ctx0, x2, x2)), x3);
+        ggml_tensor* y = ggml_mul(ctx0, ggml_mul(ctx0, ggml_mul(ctx0, x1, x1), ggml_mul(ctx0, x2, x2)), x3);
 
-    //    ggml_cgraph gf = ggml_build_forward(y);
-    //    ggml_cgraph gb = ggml_build_backward(ctx0, &gf, false);
+        ggml_cgraph gf = ggml_build_forward(y);
+        ggml_cgraph gb = ggml_build_backward(ctx0, &gf, false);
 
-    //    ggml_set_f32(x1, 1.0f);
-    //    ggml_set_f32(x2, 2.0f);
-    //    ggml_set_f32(x3, 3.0f);
+        ggml_set_f32(x1, 1.0f);
+        ggml_set_f32(x2, 2.0f);
+        ggml_set_f32(x3, 3.0f);
 
-    //    ggml_graph_reset(&gf);
-    //    ggml_set_f32(y->grad, 1.0f);
+        ggml_graph_reset(&gf);
+        ggml_set_f32(y->grad, 1.0f);
 
-    //    ggml_graph_compute(ctx0, &gb);
+        ggml_graph_compute(ctx0, &gb);
 
-    //    Console.WriteLine("y      = {0:F6}", ggml_get_f32_1d(y, 0));
-    //    Console.WriteLine("df/dx1 = {0:F6}", ggml_get_f32_1d(x1->grad, 0));
-    //    Console.WriteLine("df/dx2 = {0:F6}", ggml_get_f32_1d(x2->grad, 0));
-    //    Console.WriteLine("df/dx3 = {0:F6}", ggml_get_f32_1d(x3->grad, 0));
+        Console.WriteLine("y      = {0:F6}", ggml_get_f32_1d(y, 0));
+        Console.WriteLine("df/dx1 = {0:F6}", ggml_get_f32_1d(x1->grad, 0));
+        Console.WriteLine("df/dx2 = {0:F6}", ggml_get_f32_1d(x2->grad, 0));
+        Console.WriteLine("df/dx3 = {0:F6}", ggml_get_f32_1d(x3->grad, 0));
 
-    //    Debug.Assert(ggml_get_f32_1d(y, 0)        == 12.0f);
-    //    Debug.Assert(ggml_get_f32_1d(x1->grad, 0) == 24.0f);
-    //    Debug.Assert(ggml_get_f32_1d(x2->grad, 0) == 12.0f);
-    //    Debug.Assert(ggml_get_f32_1d(x3->grad, 0) == 4.0f);
+        Debug.Assert(ggml_get_f32_1d(y, 0) == 12.0f);
+        Debug.Assert(ggml_get_f32_1d(x1->grad, 0) == 24.0f);
+        Debug.Assert(ggml_get_f32_1d(x2->grad, 0) == 12.0f);
+        Debug.Assert(ggml_get_f32_1d(x3->grad, 0) == 4.0f);
 
-    //    ggml_tensor * g1 = x1->grad;
-    //    ggml_tensor * g2 = x2->grad;
-    //    ggml_tensor * g3 = x3->grad;
+        ggml_tensor* g1 = x1->grad;
+        ggml_tensor* g2 = x2->grad;
+        ggml_tensor* g3 = x3->grad;
 
-    //    ggml_cgraph gbb = ggml_build_backward(ctx0, &gb, true);
+        ggml_cgraph gbb = ggml_build_backward(ctx0, &gb, true);
 
-    //    ggml_graph_reset(&gb);
-    //    ggml_set_f32(g1->grad, 1.0f);
-    //    ggml_set_f32(g2->grad, 1.0f);
-    //    ggml_set_f32(g3->grad, 1.0f);
+        ggml_graph_reset(&gb);
+        ggml_set_f32(g1->grad, 1.0f);
+        ggml_set_f32(g2->grad, 1.0f);
+        ggml_set_f32(g3->grad, 1.0f);
 
-    //    ggml_graph_compute(ctx0, &gbb);
+        ggml_graph_compute(ctx0, &gbb);
 
-    //    Console.WriteLine("H * [1, 1, 1] = [ {0:F6} {1:F6} {2:F6} ]\n",
-    //            ggml_get_f32_1d(x1->grad, 0),
-    //            ggml_get_f32_1d(x2->grad, 0),
-    //            ggml_get_f32_1d(x3->grad, 0));
+        Console.WriteLine("H * [1, 1, 1] = [ {0:F6} {1:F6} {2:F6} ]\n",
+                ggml_get_f32_1d(x1->grad, 0),
+                ggml_get_f32_1d(x2->grad, 0),
+                ggml_get_f32_1d(x3->grad, 0));
 
-    //    Debug.Assert(ggml_get_f32_1d(x1->grad, 0) == 56.0f);
-    //    Debug.Assert(ggml_get_f32_1d(x2->grad, 0) == 34.0f);
-    //    Debug.Assert(ggml_get_f32_1d(x3->grad, 0) == 12.0f);
+        Debug.Assert(ggml_get_f32_1d(x1->grad, 0) == 56.0f);
+        Debug.Assert(ggml_get_f32_1d(x2->grad, 0) == 34.0f);
+        Debug.Assert(ggml_get_f32_1d(x3->grad, 0) == 12.0f);
 
-    //    ggml_graph_dump_dot(&gf, null, "test1-4-forward.dot");
-    //    ggml_graph_dump_dot(&gb, &gf,  "test1-4-backward.dot");
-    //}
+        ggml_graph_dump_dot(&gf, null, "test1-4-forward.dot");
+        ggml_graph_dump_dot(&gb, &gf, "test1-4-backward.dot");
+    }
 
     ///////////////////////////////////////////////////////////////
 
